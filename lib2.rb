afb37c48d81cba92da16bd5c2aa14d17f4aa28fa
Gnezd@home <edwindzeng@gmail.com>
2020-04-06 06:58:44 +0200

    spectral binning function

--- lib2.rb
+++ lib2.rb
@@ -12,6 +12,23 @@ def display_bytes(str, delim)
 return ret.join(delim)
 end
 
+def bin_x(range_begin, range_end, increment, ms_func)
+
+    width = ((range_end-range_begin)/increment).ceil
+    binned = Array.new(ms_func.spect.size) {Array.new(width) {0}}
+    spect_value = [0] + (0..width).map {|x| range_begin+x*increment}
+    (1..ms_func.size-1).each do |i|
+        ms_func.spect[i].each_index do |spect|
+            x = ((ms_func.spect[i][spect]-range_begin)/increment).ceil
+            break if x > width-1
+            next if x <= 0
+            binned[i][x] += ms_func.counts[i][spect]
+        end
+	end
+	
+	return binned, spect_value
+end
+
 
 class Masslynx_Function
 	
@@ -31,60 +48,26 @@ class Masslynx_Function
 		@size = idx_raw.size / 22 #number of scan time points
 		
 		@spect = Array.new(@size) {Array.new() {0.0}}
-		@counts = Array.new(@size) {Array.new(2500) {0}}
+		@counts = Array.new(@size) {Array.new(0) {0}}
 		@retention_time = Array.new(@size) {0.0}
-		raw_size_chk = 0 #from IDX cross check
 		scan_num = 0
 		while scan_num < @size
 			scan_begin, scan_size, accumulate, rt = idx_raw[22*scan_num, 22].unpack("L S x x f f") 
 			@retention_time[scan_num] = rt
-			raw_size_chk += scan_size #accumulate number of total ms data points for cross check
-			
 			#extract DAT
 			(0..scan_size-1).each do |spect|
 				raw_count, byte3, raw_mcr, raw_mcr256 = dat_raw[scan_begin + spect*6, 6].unpack("s C C S")
 				#count = raw_count * count_gain
 				#mcr = mcr_multiplier * (rawmcr256/256+rawmcr)
-#				@scans[scan_num][spect] = [((2**((byte3/16).floor-7))*(raw_mcr256*256+raw_mcr)-46384)/131034, raw_count * (4**(byte3 % 16))]
 				@spect[scan_num][spect] = ((2**((byte3/16).floor-7))*(raw_mcr256*256+raw_mcr)-46384).to_f/131034
 				@counts[scan_num][spect] = raw_count * (4**(byte3 % 16))
 			end
 			scan_num += 1
 		end
-=begin
-		counter = 0
-		while counter < dat_raw.size - 1 #6B data point loop
-			#prep and unpacks
-			raw6b = dat_raw[counter..counter+5]
-			raw_count = raw6b[0..1].unpack('s')[0]
-			count_gain = 4**(raw6b[2].unpack('C')[0] % 16)
-			mcr_multiplier = 2**((raw6b[2].unpack('C')[0]/16).floor-7)
-			raw_mcr = ("\0"+raw6b[3..5]).unpack('L')[0]/256 #mcr = mass-charge ratio
-			#real calc
-			@count.push(raw_count * count_gain)
-			@spectral_x.push((raw_mcr * mcr_multiplier - 46384).to_f / 131034) #131034 as slope and 46384 as intercept from emperical fitting. See note.md
-			counter += 6
-		end # end 6B data point loop
-=end
 	end
 
 	def inspect
 		puts ""
 	end
 
-end
-
-class Masslynx_scan
-	attr_reader :idx_raw, :scan_begin, :size, :retention_time, :accumulate, :spectral_x, :count
-
-	def initialize(idx_raw_22b, dat_raw)
-		#process IDX
-
-		#process DAT
-		@spectral_x = Array.new
-		@count = Array.new
-
-		dat_raw = dat_raw[@scan_begin..@scan_begin + @size*6] #chop in scan chunk
-		
-	end
 end
--- test.rb
+++ test.rb
@@ -3,68 +3,38 @@ load "./lib2.rb"
 raise "Filename!" if ARGV[0] == nil
 raw_name = ARGV[0]
 
-puts "Constructing FUNC1"
+puts "Constructing FUNCTIONS"
 
-time_per_func = Benchmark.measure do
+time_funcs = Benchmark.measure do
     $f1 = Masslynx_Function.new(raw_name, 1)
+    $f2 = Masslynx_Function.new(raw_name, 2)
+    $f3 = Masslynx_Function.new(raw_name, 3)
 end
 
-puts "Took #{time_per_func}"
+puts "Took #{time_funcs}"
 
-fo = File.new "output", "w"
-fo.puts $f1.retention_time
-fo.close
 #Matrix binning in spectral range!
+time_smart_bin = Benchmark.measure do
 
-#scan_ext = [$f1.scans[100], $f1.scans[200], $f1.scans[300]] # to make dev test faster
-xrange = [100, 500, 0.5]
-=begin
-binned = Array.new(scan_ext.size) {Array.new} 
-bin = 0
-time_stupid_bin = Benchmark.measure do
-    scan_ext.each_index do |i|
-        #puts "binning scan #{i}"
-        binned[i][0] = scan_ext[i].retention_time
-        x = xrange[0]
-        while x < xrange[1]
-            scan_ext[i].spectral_x.each_index do |spect| #iterate spectral point
-                if scan_ext[i].spectral_x[spect] >= (x + xrange[2])
-                    #puts "break! i=#{i} at x = #{x} becaus #{scan_ext[i].spectral_x[spect]} exceeded #{x + xrange[2]}"
-                    x += xrange[2] #iterate x frame before break!
-                    break
-                end
-            if scan_ext[i].spectral_x[spect] >= x
-                bin += scan_ext[i].count[spect] 
-                #puts "bin! #{i}"
-            end
-            end #end spectral iteration
-            x += xrange[2]
-            binned[i].push bin
-            bin = 0        
-        end
-    end
-    martrix_o = File.open("matrix.csv", "w")
+    binned, spect_values = bin_x(100,500,0.5, $f1)
+    martrix_o = File.open("func1.csv", "w")
+    martrix_o.puts spect_values.join ","
     binned.each do |row|
         martrix_o.puts row.join(", ")
     end
     martrix_o.close
-end
-puts "Stupid binning took: #{time_stupid_bin}"
-=end
-#=begin
-time_smart_bin = Benchmark.measure do
-    width = ((xrange[1]-xrange[0])/xrange[2]).ceil
-    binned = Array.new($f1.spect.size) {Array.new(width) {0}}
-    binned[0]= [0] + (0..width).map {|x| xrange[0]+x*xrange[2]}
-    (1..$f1.size-1).each do |i|
-        $f1.spect[i].each_index do |spect|
-            x = (($f1.spect[i][spect]-xrange[0])/xrange[2]).ceil
-            break if x > width-1
-            next if x <= 0
-            binned[i][x] += $f1.counts[i][spect]
-        end
+    
+    binned, spect_values = bin_x(100,500,0.5, $f2)
+    martrix_o = File.open("func2.csv", "w")
+    martrix_o.puts spect_values.join ","
+    binned.each do |row|
+        martrix_o.puts row.join(", ")
     end
-    martrix_o = File.open("matrix_smart.csv", "w")
+    martrix_o.close
+
+    binned, spect_values = bin_x(210, 600, 1, $f3)
+    martrix_o = File.open("func3.csv", "w")
+    martrix_o.puts spect_values.join ","
     binned.each do |row|
         martrix_o.puts row.join(", ")
     end
